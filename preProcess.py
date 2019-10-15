93cd63ec115e1818ca2a0a3b1912dfd371d63f66
Cai Runze <cairunze17@mails.ucas.ac.cn>
2019-10-16 00:01:12 +0800

    update dict.txt

--- preProcess.py
+++ preProcess.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import train_test_split 
 import pandas as pd
 import re
 
--- wordCut.py
+++ wordCut.py
@@ -2,6 +2,7 @@ import pandas as pd
 import jieba
 import gensim
 # from snownlp import SnowNLP
+# from sklearn.model_selection import train_test_split
 import re
 
 def snow_result(comment):
@@ -24,6 +25,8 @@ def cutData(filePath):
 
 
 if __name__ =='__main__':
+	jieba.load_userdict('dict.txt')
+	jieba.enable_parallel(2)
 	cutData = cutData('Train/preprocessed_train_data.csv')
 
 	test= "问责领导(上黄镇党委书记张涛，宣国才真能一手遮天吗？),\"这几天看了有人举报施某某的贴子，经与举报人联系证实，是宣某当天中午请举报人和枪手喝酒后，晚上才发的贴子！本人不去讨论前二天的举报，相信总归会有说法的！今天一看施全军2017年1月2日实名举报上黄镇宣国才的贴子（仍被锁定禁止评论）已经正好一整年了=750)this.width='750';""src=\"\"\"\"style=\"\"max-width:750px;\"\"/>图片:/home/alidata/www/data/tmp/qfupload/4_291085_1514981471478952.jpg施全军实名"
