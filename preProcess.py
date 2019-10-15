23b5c2d3191f00e2da1f902d6d2d5307689c35db
Cai Runze <cairunze17@mails.ucas.ac.cn>
2019-10-15 14:08:35 +0800

    add something

--- preProcess.py
+++ preProcess.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import re
 
+
 def deleteNoneSense(text):
-	html=re.compile('<.*?>')
-	text = re.sub(html,'',text)
-	text = re.sub('[\s]*','',text)
+	html  = re.compile('<.*?>')
+	http  = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+	more  = re.compile(r'\b(?!src|href)\w+=[\'\"].*?[\'\"](?=[\s\>])')
+	space = re.compile(r'\s')
+	someid= re.compile(r'(id:.*)')
+	text  = re.sub(html,'',text)
+	text  = re.sub(http,'',text)
+	text  = re.sub(space,'',text)
+	text  = re.sub('window.open','',text)
+	text  = re.sub(more,'',text)
 	return text
 
 def loadData(train_filePath,label_filePath):
@@ -22,5 +30,9 @@ def loadData(train_filePath,label_filePath):
 if __name__ =='__main__':
 	trainData=loadData('Train/Train_DataSet_Label.csv','Train/Train_DataSet.csv')
 	trainData.to_csv('Train/preprocessed_train_data.csv')
+	test= 'https://myappple.comshu.cn/cn/123 3413 (id:1001蔡润泽)okthen'
+	deleteNoneSense(test)
+	print(test)
+
 	
 		
