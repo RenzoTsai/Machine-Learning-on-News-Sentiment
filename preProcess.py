95652f777307dc4f08f8828a9d57401fe01783cd
Cai Runze <cairunze17@mails.ucas.ac.cn>
2019-10-15 14:46:21 +0800

    update preProcess.py

--- preProcess.py
+++ preProcess.py
@@ -5,14 +5,19 @@ import re
 def deleteNoneSense(text):
 	html  = re.compile('<.*?>')
 	http  = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-	more  = re.compile(r'\b(?!src|href)\w+=[\'\"].*?[\'\"](?=[\s\>])')
-	space = re.compile(r'\s')
-	someid= re.compile(r'(id:.*)')
+	src  = re.compile(r'\b(?!src|href)\w+=[\'\"].*?[\'\"](?=[\s\>])')
+	space = re.compile(r'\n')
+	ids= re.compile(r'\(["微信"]?id:(.*)\)')
+	others= re.compile(u'[^\u4e00-\u9fa5\u0041-\u005A\u0061-\u007A\u0030-\u0039\u3002\uFF1F\uFF01\uFF0C\u3001\uFF1B\uFF1A\u300C\u300D\u300E\u300F\u2018\u2019\u201C\u201D\uFF08\uFF09\u3014\u3015\u3010\u3011\u2014\u2026\u2013\uFF0E\u300A\u300B\u3008\u3009\!\@\#\$\%\^\&\*\(\)\-\=\[\]\{\}\\\|\;\'\:\"\,\.\/\<\>\?\/\*\+\_"\u0020]+')
+
 	text  = re.sub(html,'',text)
 	text  = re.sub(http,'',text)
 	text  = re.sub(space,'',text)
 	text  = re.sub('window.open','',text)
-	text  = re.sub(more,'',text)
+	text  = re.sub(src,'',text)
+	text  = re.sub(ids,'',text)
+	text  = re.sub(others,'',text)
+	text  = re.sub("\\n",'',text)
 	return text
 
 def loadData(train_filePath,label_filePath):
@@ -30,8 +35,10 @@ def loadData(train_filePath,label_filePath):
 if __name__ =='__main__':
 	trainData=loadData('Train/Train_DataSet_Label.csv','Train/Train_DataSet.csv')
 	trainData.to_csv('Train/preprocessed_train_data.csv')
-	test= 'https://myappple.comshu.cn/cn/123 3413 (id:1001蔡润泽)okthen'
-	deleteNoneSense(test)
+
+	test = "(微信)《fine》(hhh123)\nhao this is a htttp: https://blog.csdn.net/hawkzy/article/details/85110213 (id:1001蔡润泽)okthen ►"
+	test = deleteNoneSense(test)
+
 	print(test)
 
 	
