7202b892a1c95876f58fd59377e077eb1170492d
Cai Runze <cairunze17@mails.ucas.ac.cn>
2019-10-16 21:26:46 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,8 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-import matplotlib.pyplot as plt
+import sklearn
+#import matplotlib.pyplot as plt
 
 def get_custom_stopwords(stop_words_file):
     with open(stop_words_file) as sw:
@@ -21,6 +22,7 @@ if __name__ =='__main__':
 	X = trainData['content'].astype('U')
 	y = trainData.label
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2019)
+
 	Vectorizer = CountVectorizer( max_df = 0.8,
                             	  min_df = 3,
                             	  token_pattern = u'(?u)\\b[^\\d\\W]\\w+\\b',
@@ -36,6 +38,9 @@ if __name__ =='__main__':
 
 	X_test_vect = Vectorizer.transform(X_test)
 	print("content test score is : ", nb.score(X_test_vect, y_test))
+
+	y_predict = nb.predict(Vectorizer.transform(X_test))
+	print("content test macro f1_score:",sklearn.metrics.f1_score(y_test, y_predict, average='macro')) 
 
 	print("Apply to Test Data...")
 	testData = pd.read_csv('Test/result.csv',index_col=0)
@@ -59,14 +64,22 @@ if __name__ =='__main__':
 	print(Vectorizer_Title.fit_transform(X_train))
 	nb.fit(X_train_vect, y_train)
 	train_score = nb.score(X_train_vect, y_train)
+	
 	print("title train score is : ",train_score)
+
 
 	X_test_vect = Vectorizer_Title.transform(X_test)
 	print("title test score is : ", nb.score(X_test_vect, y_test))
 
+	y_predict = nb.predict(Vectorizer_Title.transform(X_test))
+	print("title test macro f1_score:",sklearn.metrics.f1_score(y_test, y_predict, average='macro'))  
+
 
 	print("Apply to Test Data...")
+
 	testResult = nb.predict(Vectorizer_Title.transform(testData['title'].astype('U')))
+
+
 	testData['label_title'] = testResult
 	testData.to_csv ('Test/result.csv')
 
